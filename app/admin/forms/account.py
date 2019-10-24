3f26a258c51170dd16eb4a7b640135da4b72b3b7
ezkat <50319957+ezkat@users.noreply.github.com>
2019-10-24 14:07:40 +0300

    Account control

--- app/admin/forms/account.py
+++ app/admin/forms/account.py
@@ -1,8 +1,10 @@
 from wtforms_alchemy import ModelForm
-from wtforms import SelectField
+from wtforms import SelectField, SelectField
 from app.utils.models.account import Account
 from app.utils.models.rank import ranks
 
 class AccountForm(ModelForm):
+    rank                    =   SelectField('Rank', choices=tuple([(k, ranks[k]) for k in ranks]))
+
     class Meta:
         model = Account
--- app/admin/routes/account.py
+++ app/admin/routes/account.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, redirect, request, url_for
 from flask_login import login_required, current_user
 
-from app.utils.models.account import list_accounts, find_account, Account, amount
+from app.utils.models.account import list_accounts, find_account, Account, amount, find_account_by_username, find_account_by_email
 from app.utils.models.rank import ranks
 from app.admin.forms.account import AccountForm
 from app.managers.password import generate_hash_pass
@@ -26,18 +26,44 @@ def accounts():
 def account():
     _type = request.args.get('type')
     id = request.args.get('id')
+    print(_type, id)
     if _type == 'edit':
         if id == 0:
             return redirect(url_for('account.accounts'))
         account = find_account(id)
         if account:
-            return render_template('admin/pages/accounts/account.html', account=account, form=AccountForm(), ranks=ranks, render_type='edit')
+            print('Editing account (%s) %s, request by: %s' % (account.id, account.username, current_user.username))
+            return render_template('admin/pages/accounts/account.html', account=account, form=AccountForm(), render_type='edit', redirect=redirect)
         else:
             return render_template('admin/pages/404.html', reason='Account', content='Not found')
     elif _type == 'add':
         form = AccountForm(request.form)
+        
+        if not form.username.data or form.username.data is None or len(form.username.data) < 3:
+            return json.dumps({
+                'success':False,
+                'message':'Invalid username value'
+            }), 400, {'ContentType':'application/json'}
 
-        print(request.form.get('selected'))
+        if not form.password.data or form.password.data is None or len(form.password.data) < 6:
+            return json.dumps({
+                'success':False,
+                'message':'Invalid password value'
+            }), 400, {'ContentType':'application/json'}
+
+        if not form.email.data or form.email.data is None or len(form.email.data) < 6:
+            return json.dumps({
+                'success':False,
+                'message':'Invalid email value'
+            }), 400, {'ContentType':'application/json'}
+
+        
+        account = find_account_by_username(form.username.data) or find_account_by_email(form.email.data)
+        if account:
+            return json.dumps({
+                'success':False,
+                'message':'Account already exists.'
+            }), 400, {'ContentType':'application/json'}
 
         account = Account(
             username=form.username.data,
@@ -46,11 +72,25 @@ def account():
             online=0
         )
         account.change_rank(
-            level=0
+            level=int(form.rank.data)
         )
         account.save()
-        return redirect(url_for('account.account', type='edit', id=account.id))
+        return json.dumps({
+            'success':True
+        }), 200, {'ContentType':'application/json'}
     elif _type == 'new':
         return render_template('admin/pages/accounts/account.html', form=AccountForm(), ranks=ranks, render_type='new')
+    elif _type == 'delete':
+        account = find_account(id)
+        if account:
+            print('Username: %s' % account.username)
+            account.delete()
+            return redirect(url_for('account.accounts'))
+        else:
+            return json.dumps({
+                'success':False,
+                'message':'Account not found.'
+            }), 400, {'ContentType':'application/json'}
+
     else:
         return redirect(url_for('account.accounts'))
