996ea874e75222ad11d9b9628ba314f544a4b999
ezkat <50319957+ezkat@users.noreply.github.com>
2019-10-30 13:29:33 +0200

    Update importdb.py

--- app/utils/extensions/importdb.py
+++ app/utils/extensions/importdb.py
@@ -4,8 +4,19 @@ import json, requests
 from sys import exit, argv
 from os.path import exists
 from app.services.models.service import Service, get_service_by_name, get_category_item_by_name
-from app.services.models.category import get_categories, get_relation_type, get_category_by_category_items, Category, CategoryItems
+from app.services.models.category import get_categories, get_relation_type, get_category_by_category_items, Category, CategoryItems, get_category_by_name
 from sqlalchemy.exc import DataError
+
+CATEGORY_IDS = {
+    'target': 'Neuvonta- ja ohjauspalvelut',
+    'service_path': 'Koulutuspalvelut',
+    'classification': 'Työllistymisedellytyksiä lisäävät palvelut',
+    'immigration': 'Työnhaku- ja työnvälityspalvelut',
+    'age_group': 'Yrittäjyyspalvelut',
+    'unemployment_duration': 'Terveys-, kuntoutus- ja sosiaalipalvelut',
+    'health': 'Muut palvelut',
+    'integration': 'Palveluja kohderyhmittäin'
+}
 
 class Import:
     def __init__(self):
@@ -47,7 +58,11 @@ class Import:
                                         cgItem = get_category_item_by_name(item)
                                         cgs = dict(enumerate(get_categories()))
                                         if not cgItem or cgItem is None:
-                                            category = get_category_by_category_items(item)
+                                            print('Look up for category: %s' % value)
+                                            try:
+                                                category = get_category_by_name(CATEGORY_IDS[value])
+                                            except:
+                                                category = get_category_by_category_items(item)
                                             if not category or category is None:
                                                 x = True
                                                 while x:
@@ -90,7 +105,10 @@ class Import:
                                                 ).save()
                                                 print('Added %s to category %s' % (item, category.name))
                                         else:
-                                            category = get_category_by_category_items(cgItem.text)
+                                            try:
+                                                category = get_category_by_name(CATEGORY_IDS[value])
+                                            except:
+                                                category = get_category_by_category_items(cgItem.text)
                                             if not category or category is None:
                                                 x = True
                                                 while x:
