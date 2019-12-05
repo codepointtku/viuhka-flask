cd152a790b323dffe39903c2a5541747befc53de
ezkat <50319957+ezkat@users.noreply.github.com>
2019-12-05 07:51:06 +0200

    Requirements

--- app/javascripts/routes/getAmount.py
+++
@@ -1,9 +0,0 @@
-from flask                              import Blueprint
-
-module = Blueprint('javascripts', __name__)
-
-_name_ = 'JSScripts'
-
-@module.route('/getAmount')
-def get_amount():
-    return amount()
