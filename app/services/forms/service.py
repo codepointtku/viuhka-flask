ef8f5e16c7b4f608ee61a4b4e1b61201d18da871
ezkat <50319957+ezkat@users.noreply.github.com>
2019-12-10 10:46:34 +0200

    Publish fix

--- app/services/forms/service.py
+++ app/services/forms/service.py
@@ -1,5 +1,5 @@
 from wtforms_alchemy                                import ModelForm, QuerySelectMultipleField
-from wtforms                                        import SelectMultipleField, IntegerField
+from wtforms                                        import SelectMultipleField, IntegerField, BooleanField
 from wtforms.fields.html5                           import DateTimeLocalField
 
 from wtforms.validators                             import Required
@@ -14,6 +14,8 @@ class ServiceForm(ModelForm, Form):
     search_result_priority      =   IntegerField(label="Search result priority")
     start                       =   DateTimeLocalField(label="Start", format='%Y-%m-%dT%H:%M', validators=[Required()])
     end                         =   DateTimeLocalField(label="End",   format='%Y-%m-%dT%H:%M', validators=[Required()])
+    published                   =   BooleanField(label="Published", default=False)
+    open_ended                  =   BooleanField(label="Open ended", default=False)
 
     class Meta:
         model = Service
